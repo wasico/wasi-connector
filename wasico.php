cfc0385c752c056cc7a09647b118c4f593968099
Elkin Ossa <eossa93@hotmail.com>
2023-08-29 12:33:38 -0500

    fix (general): version not updated
    Update the version to 2.0.2.

--- wasico.php
+++ wasico.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Wasi Connector
  * Plugin URI:        https://api.wasi.co/
  * Description:       Plugin to convert your website into a Real Estate Listing site using your properties on Wasi.co
- * Version:           2.0.1
+ * Version:           2.0.2
  * Author:            WasiCo, Inc
  * Author URI:        wasi.co
  * License:           Commercial
@@ -26,7 +26,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  */
-define( 'WASICO_VERSION', '2.0.1' );
+define( 'WASICO_VERSION', '2.0.2' );
 
 /**
  * The code that runs during plugin activation.
